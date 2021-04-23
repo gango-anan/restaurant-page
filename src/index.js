a23d939ef80f9330091dbebd34384596b8c0869a
Galiwango Ananiya <galiwango101@gmail.com>
2021-04-23 21:11:34 +0300

    Add functionality to 'add and display about us content page.'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import { onPageLoad } from './onpageload'
+import { createAboutUsContent } from './aboutus'
+
 const mainContentElement = document.getElementById('content');
 window.onload = onPageLoad(mainContentElement);
+
+// Utility functions.
+function wipeExistingContents(parentContainer) {
+  parentContainer.removeChild(parentContainer.lastChild);
+}
+
+// Event Listeners.
+mainContentElement.addEventListener('click', (e) => {
+  if (e.target && e.target.matches('li#menu-item1')) {
+    createAboutUsContent(mainContentElement);
+  }
+  else if (e.target && e.target.matches('li#menu-item2')) {
+    createMenuContent(mainContentElement);
+  }
+  else if (e.target && e.target.matches('li#menu-item3')) {
+    createContactContent(mainContentElement);
+  }
+});
+
+
